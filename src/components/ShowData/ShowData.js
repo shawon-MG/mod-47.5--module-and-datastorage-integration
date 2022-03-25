7e890eb7914152e1d3a48969b1d860b1ce486046
shawon-MG <work.shawomg@gmail.com>
2022-03-25 22:33:07 +0600

    event handler added using arrow func wrapped in onClick

--- src/components/ShowData/ShowData.js
+++ src/components/ShowData/ShowData.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 const ShowData = (props) => {
     const {id, eyeColor, name, gender} = props.shops;
+
+    const addId = (id) => console.log(id);
+
     return (
         <div style={{padding: '15px', margin: '20px', borderRadius: '20px', border: '1px solid black'}}>
             <h3>ID: {id}</h3>
             <h3>Name: {name}</h3>
             <h3>Eye: {eyeColor}</h3>
             <h3>Gender: {gender}</h3>
+            <button onClick={() => addId(id)}>ID Clicked</button>
         </div>
     );
 };
